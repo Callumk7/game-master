1e54737536cbd45681f67955e358e425100ab832
Callum Kloos <callumkloos@gmail.com>
2024-05-08 10:04:08 +0100

    added dependencies

--- apps/game-master-server/src/index.ts
+++ apps/game-master-server/src/index.ts
@@ -1,9 +1,45 @@
-import { Hono } from 'hono'
+import { zValidator } from "@hono/zod-validator";
+import { logger } from 'hono/logger';
+import { Hono } from "hono";
+import { sign } from "hono/jwt";
+import { z } from "zod";
+import { allEntitiesRoute } from "~/routes/all";
+import { charactersRoute } from "~/routes/characters";
+import { factionsRoute } from "./routes/factions";
+import { notesRoute } from "./routes/notes";
 
-const app = new Hono()
+export type Bindings = {
+	TURSO_CONNECTION_URL: string;
+	TURSO_AUTH_TOKEN: string;
+	AUTH_KEY: string;
+};
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono<{ Bindings: Bindings }>();
 
-export default app
+app.get("/", (c) => {
+	return c.text("YOU DID IT");
+});
+
+app.use(logger())
+
+app.route("/notes", notesRoute);
+app.route("/all", allEntitiesRoute);
+app.route("/characters", charactersRoute);
+app.route("/factions", factionsRoute);
+
+app.post(
+	"/auth",
+	zValidator(
+		"json",
+		z.object({
+			body: z.string(),
+		}),
+	),
+	async (c) => {
+		const { body } = c.req.valid("json");
+		const token = await sign({ user: body }, c.env.AUTH_KEY);
+		return c.text(token);
+	},
+);
+
+export default app;
