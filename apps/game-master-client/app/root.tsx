3fb936bec4170a6fae86de69a15cb39bf33fc437
Callum Kloos <callumkloos@gmail.com>
2024-05-16 11:12:17 +0100

    setup

---
+++ apps/game-master-client/app/root.tsx
@@ -0,0 +1,37 @@
+import {
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+	useNavigate,
+} from "@remix-run/react";
+import { RouterProvider } from "react-aria-components";
+import "./tw.css";
+
+export function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body className="dark bg-grade-1 text-grade-12">
+				{children}
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	);
+}
+
+export default function App() {
+	const navigate = useNavigate();
+	return (
+		<RouterProvider navigate={navigate}>
+			<Outlet />
+		</RouterProvider>
+	);
+}
