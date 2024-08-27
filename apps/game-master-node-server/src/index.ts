064aefb0c2df38263713ef77ea36a7eb5cd63886
Callum Kloos <Callumkloos@gmail.com>
2024-08-27 13:56:41 +0100

    unfinished: start structuring node app

--- apps/game-master-node-server/src/index.ts
+++ apps/game-master-node-server/src/index.ts
@@ -4,15 +4,33 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+	return c.text('Hello Hono!')
 })
 
 const port = 3000
 console.log(`Server is running on port ${port}`)
 
 serve({
-  fetch: app.fetch,
-  port
+	fetch: app.fetch,
+	port
 })
 
 export default app;
+
+
+////////////////////////////////////////////////////////////////////////////////
+//                                Routes
+////////////////////////////////////////////////////////////////////////////////
+
+const userRoute = new Hono()
+
+userRoute.get("/", (c) => c.text("This is the user route, please use the correct endpoint"))
+
+userRoute.get("/:userId", async (c) => {
+	// validate request shape
+
+	// access correct user data
+	// return user data
+})
+
+
