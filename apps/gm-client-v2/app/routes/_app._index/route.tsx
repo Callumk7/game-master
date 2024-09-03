eb5b34410d7905538379c4e2cf9e006aba74e4b2
Callum Kloos <Callumkloos@gmail.com>
2024-09-03 14:11:03 +0100

    clear out monorepo apps

--- apps/gm-client-v2/app/routes/_app._index/route.tsx
+++
@@ -1,3 +0,0 @@
-export default function AppIndex() {
-  return <p>This is the app index route</p>
-}
--- apps/gm-client-v2/app/routes/_app.notes/route.tsx
+++
@@ -1,3 +0,0 @@
-export default function NotesLayout() {
-  return <p>This is the notes layout route</p>
-}
