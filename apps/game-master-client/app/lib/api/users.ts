3e9d92d8cfe35ed01dabe30de161e1df857d29d5
Callum Kloos <Callumkloos@gmail.com>
2024-09-06 14:32:20 +0100

    experimenting with shared type package

--- apps/game-master-client/app/lib/api/users.ts
+++ apps/game-master-client/app/lib/api/users.ts
@@ -1,10 +1,10 @@
-import type { User } from "types/users";
 import type { Client } from ".";
+import type { GetUserResponse } from "@repo/shared-types";
 
 export class Users {
 	constructor(private client: Client) {}
 
-	async getUser(id: string): Promise<User> {
-		return this.client.get<User>(`users/${id}`);
+	async getUser(id: string): Promise<GetUserResponse> {
+		return this.client.get<GetUserResponse>(`users/${id}`);
 	}
 }
