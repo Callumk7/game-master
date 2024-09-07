32f261a4002c8836e108ebd0d5e3b48f9d9c4d32
Callum Kloos <Callumkloos@gmail.com>
2024-09-07 12:01:40 +0100

    editor etc

---
+++ apps/game-master-client/app/components/editor/index.tsx
@@ -0,0 +1,64 @@
+import {
+  useEditor,
+  EditorContent,
+  BubbleMenu,
+  type Editor,
+} from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { FontBoldIcon, FontItalicIcon, HeadingIcon } from "@radix-ui/react-icons";
+import { Toolbar } from "~/ui/toolbar";
+import { Button } from "~/ui/button";
+
+// define your extension array
+const extensions = [StarterKit];
+
+const content = "<p>Hello World!</p>";
+
+export function EditorBody() {
+  const editor = useEditor({
+    extensions,
+    content,
+  });
+
+  return (
+    <>
+      <EditorContent editor={editor} />
+      {editor && (
+        <BubbleMenu editor={editor}>
+          <BubbleMenuItems editor={editor} />
+        </BubbleMenu>
+      )}
+    </>
+  );
+}
+
+interface BubbleMenuItemsProps {
+  editor: Editor;
+}
+export function BubbleMenuItems({ editor }: BubbleMenuItemsProps) {
+  return (
+    <Toolbar className="p-3 bg-background/80 backdrop-blur rounded-md">
+      <Button
+        size="icon"
+        variant="secondary"
+        onPress={() => editor.chain().focus().toggleBold().run()}
+      >
+        <FontBoldIcon />
+      </Button>
+      <Button
+        size="icon"
+        variant="secondary"
+        onPress={() => editor.chain().focus().toggleItalic().run()}
+      >
+        <FontItalicIcon />
+      </Button>
+      <Button
+        size="icon"
+        variant="secondary"
+        onPress={() => editor.chain().focus().toggleHeading({level: 1}).run()}
+      >
+        <HeadingIcon />
+      </Button>
+    </Toolbar>
+  );
+}
--- apps/game-master-client/app/routes/_app._index/route.tsx
+++ apps/game-master-client/app/routes/_app._index/route.tsx
@@ -1,48 +1,31 @@
-import type { MetaFunction } from "@remix-run/node";
+import { json, type LoaderFunctionArgs, type MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { EditorBody } from "~/components/editor";
+import { api } from "~/lib/api";
+import { validateUser } from "~/lib/auth.server";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Game Master: Notes for Heroes" },
+    {
+      name: "description",
+      content: "Take your notes to the next level with Game Master",
+    },
   ];
 };
 
+export const loader = async ({ request, params, context }: LoaderFunctionArgs) => {
+  const userId = await validateUser(request);
+  const ownedGames = await api.games.getOwnedGames(userId);
+  return json({ ownedGames });
+};
+
 export default function Index() {
+  const { ownedGames } = useLoaderData<typeof loader>();
   return (
     <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1>Index</h1>
+      <EditorBody />
     </div>
   );
 }
