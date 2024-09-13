5d53e334c484717f4524696e00edca099fc9a548
Callum Kloos <Callumkloos@gmail.com>
2024-09-13 14:49:04 +0100

    remove unused import

--- apps/game-master-node-server/test/games.test.ts
+++ apps/game-master-node-server/test/games.test.ts
@@ -1,6 +1,5 @@
 import { describe, expect, it } from "vitest";
 import { gamesRoute } from "../src/features/games";
-import type { CreateGameRequestBody } from "@repo/api";
 
 describe("Games Route", async () => {
 	describe("/games route", async () => {
