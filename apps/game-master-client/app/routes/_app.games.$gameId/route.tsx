836874d00662bc9621738af78186c0c376a74e3f
Callum Kloos <Callumkloos@gmail.com>
2024-09-18 18:47:25 +0100

    game navbar

--- apps/game-master-client/app/routes/_app.games.$gameId/route.tsx
+++ apps/game-master-client/app/routes/_app.games.$gameId/route.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from "@remix-run/react";
-import { GameSettingsMenu } from "./components/game-settings-menu";
+import { GameNavbar } from "./components/game-navbar";
 
 export default function GameLayout() {
   return (
     <div className="p-4 space-y-4">
-      <GameSettingsMenu gameId="string" />
+      <GameNavbar />
       <Outlet />
     </div>
   );
