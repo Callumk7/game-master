204a71d3dd76a7be9ccfa305704d228b791c50a1
Callum Kloos <Callumkloos@gmail.com>
2024-09-20 12:23:48 +0100

    render html

---
+++ apps/game-master-client/app/components/render-html.tsx
@@ -0,0 +1,13 @@
+import { cn } from "callum-util";
+
+export function RenderHtml({
+	content,
+	className,
+}: { content: string; className?: string }) {
+	return (
+		<div
+			className={cn("prose prose-invert max-w-none", className)}
+			dangerouslySetInnerHTML={{ __html: content }}
+		/>
+	);
+}
