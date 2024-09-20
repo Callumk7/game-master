9761d734425083a1fb777cb212bdbc75fbf5e18f
Callum Kloos <Callumkloos@gmail.com>
2024-09-20 12:09:05 +0100

    add list of game notes to index

--- apps/game-master-client/app/routes/_app.games.$gameId.notes._index/route.tsx
+++ apps/game-master-client/app/routes/_app.games.$gameId.notes._index/route.tsx
@@ -1,3 +1,30 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { useParams } from "@remix-run/react";
+import { typedjson, useTypedLoaderData } from "remix-typedjson";
+import { z } from "zod";
+import { parseParams } from "zodix";
+import { Link } from "~/components/ui/link";
+import { api } from "~/lib/api.server";
+
+export const loader = async ({ request, params, context }: LoaderFunctionArgs) => {
+	const { gameId } = parseParams(params, { gameId: z.string() });
+
+	const allGameNotes = await api.notes.getAllGameNotes(gameId);
+
+	return typedjson({ allGameNotes, gameId });
+};
+
 export default function NotesIndex() {
-  return <p>This is the index</p>
+	const { allGameNotes, gameId } = useTypedLoaderData<typeof loader>();
+	return (
+		<div>
+			<div className="flex flex-col gap-3">
+				{allGameNotes.map((note) => (
+					<Link key={note.id} href={`/games/${gameId}/notes/${note.id}`} variant={"link"}>
+						{note.name}
+					</Link>
+				))}
+			</div>
+		</div>
+	);
 }
