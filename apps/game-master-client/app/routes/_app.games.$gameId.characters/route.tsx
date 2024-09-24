408d920c8e163dd5c93d4c66c72849e1e524c6b2
Callum Kloos <Callumkloos@gmail.com>
2024-09-24 15:58:34 +0200

    close sidebar on layout navigations

--- apps/game-master-client/app/routes/_app.games.$gameId.characters/route.tsx
+++ apps/game-master-client/app/routes/_app.games.$gameId.characters/route.tsx
@@ -1,6 +1,9 @@
 import { Outlet } from "@remix-run/react";
+import { useSetRightSidebarOpen } from "~/store/selection";
 
 export default function CharacterLayout() {
+  const setIsRightSidebarOpen = useSetRightSidebarOpen();
+  setIsRightSidebarOpen(false);
   return (
     <div>
       <Outlet />
