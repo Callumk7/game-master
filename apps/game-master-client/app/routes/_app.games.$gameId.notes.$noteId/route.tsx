a73213fe0b24c0568c498fa3577d3e387a0e26f3
Callum Kloos <Callumkloos@gmail.com>
2024-09-27 18:51:05 +0200

    editable text on notes, update the games editable text

--- apps/game-master-client/app/routes/_app.games.$gameId.notes.$noteId/route.tsx
+++ apps/game-master-client/app/routes/_app.games.$gameId.notes.$noteId/route.tsx
@@ -8,7 +8,7 @@ import {
 import { z } from "zod";
 import { parseForm, parseParams } from "zodix";
 import { EditorBody } from "~/components/editor";
-import { Text } from "~/components/ui/typeography";
+import { Text, EditableText } from "~/components/ui/typeography";
 import { api } from "~/lib/api.server";
 import { NoteToolbar } from "./components/note-toolbar";
 import { NoteSidebar } from "./components/note-sidebar";
@@ -16,7 +16,7 @@ import { OptionalEntitySchema } from "types/schemas";
 import { stringOrArrayToArray } from "callum-util";
 import { getNoteData } from "./queries.server";
 import { useGameData } from "../_app.games.$gameId/route";
-import type { MentionItem } from "~/types/mentions";
+import { updateNoteContentSchema } from "@repo/api";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
 	const { noteId } = parseParams(params, {
@@ -35,10 +35,7 @@ export const action = async ({ request, params }: ActionFunctionArgs) => {
 	});
 
 	if (request.method === "PATCH") {
-		const data = await parseForm(request, {
-			content: z.string(),
-			htmlContent: z.string(),
-		});
+		const data = await parseForm(request, updateNoteContentSchema);
 		const result = await api.notes.updateNote(noteId, data);
 
 		if (!result.success) {
@@ -87,7 +84,15 @@ export default function NotesRoute() {
 		<>
 			<div className="p-4 space-y-4">
 				<NoteToolbar noteId={note.id} />
-				<Text variant={"h2"}>{note.name}</Text>
+				<EditableText
+					method="patch"
+					fieldName={"name"}
+					value={note.name}
+					variant={"h2"}
+					weight={"semi"}
+					inputLabel={"Game name input"}
+					buttonLabel={"Edit game name"}
+				/>
 				<EditorBody htmlContent={note.htmlContent} suggestionItems={suggestionItems} />
 			</div>
 			<NoteSidebar />
