87997c176f409a82790e18b4fb2b2bba0009fea5
Callum Kloos <Callumkloos@gmail.com>
2024-09-27 14:23:05 +0200

    biome ignore for use effect

--- apps/game-master-client/app/components/editor/components/mention-list.tsx
+++ apps/game-master-client/app/components/editor/components/mention-list.tsx
@@ -2,70 +2,73 @@ import type { SuggestionOptions, SuggestionProps } from "@tiptap/suggestion";
 import { forwardRef, useEffect, useImperativeHandle, useState } from "react";
 
 export const MentionList = forwardRef<
-	ReturnType<NonNullable<SuggestionOptions["render"]>>,
-	SuggestionProps<{ id: string; label: string; href: string }>
+  ReturnType<NonNullable<SuggestionOptions["render"]>>,
+  SuggestionProps<{ id: string; label: string; href: string }>
 >((props, ref) => {
-	const [selectedIndex, setSelectedIndex] = useState(0);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
-	const selectItem = (index: number) => {
-		const item = props.items[index];
+  const selectItem = (index: number) => {
+    const item = props.items[index];
 
-		if (item) {
-			props.command({ id: item.id, href: item.href, label: item.label });
-		}
-	};
+    if (item) {
+      props.command({ id: item.id, href: item.href, label: item.label });
+    }
+  };
 
-	const upHandler = () => {
-		setSelectedIndex((selectedIndex + props.items.length - 1) % props.items.length);
-	};
+  const upHandler = () => {
+    setSelectedIndex(
+      (selectedIndex + props.items.length - 1) % props.items.length
+    );
+  };
 
-	const downHandler = () => {
-		setSelectedIndex((selectedIndex + 1) % props.items.length);
-	};
+  const downHandler = () => {
+    setSelectedIndex((selectedIndex + 1) % props.items.length);
+  };
 
-	const enterHandler = () => {
-		selectItem(selectedIndex);
-	};
+  const enterHandler = () => {
+    selectItem(selectedIndex);
+  };
 
-	useEffect(() => setSelectedIndex(0), [props.items]);
+  // biome-ignore lint/correctness/useExhaustiveDependencies: Safely selects the first item if the list changes
+  useEffect(() => setSelectedIndex(0), [props.items]);
 
-	useImperativeHandle(ref, () => ({
-		onKeyDown: ({ event }) => {
-			if (event.key === "ArrowUp") {
-				upHandler();
-				return true;
-			}
+  useImperativeHandle(ref, () => ({
+    onKeyDown: ({ event }) => {
+      if (event.key === "ArrowUp") {
+        upHandler();
+        return true;
+      }
 
-			if (event.key === "ArrowDown") {
-				downHandler();
-				return true;
-			}
+      if (event.key === "ArrowDown") {
+        downHandler();
+        return true;
+      }
 
-			if (event.key === "Enter") {
-				enterHandler();
-				return true;
-			}
+      if (event.key === "Enter") {
+        enterHandler();
+        return true;
+      }
 
-			return false;
-		},
-	}));
+      return false;
+    },
+  }));
 
-	return (
-		<div className="dropdown-menu">
-			{props.items.length ? (
-				props.items.map((item, index) => (
-					<button
-						className={index === selectedIndex ? "is-selected" : ""}
-						key={item.id}
-						onClick={() => selectItem(index)}
-						type="button"
-					>
-						{item.label}
-					</button>
-				))
-			) : (
-				<div className="item">No result</div>
-			)}
-		</div>
-	);
+  return (
+    <div className="dropdown-menu">
+      {props.items.length ? (
+        props.items.map((item, index) => (
+          <button
+            className={index === selectedIndex ? "is-selected" : ""}
+            key={item.id}
+            onClick={() => selectItem(index)}
+            type="button"
+          >
+            {item.label}
+          </button>
+        ))
+      ) : (
+        <div className="item">No result</div>
+      )}
+    </div>
+  );
 });
