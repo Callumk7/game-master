5bf67bae7ffc762dbff32c2243a49ee883e7e03c
Callum Kloos <Callumkloos@gmail.com>
2024-10-03 11:43:10 +0100

    refresh migrations to up to date schema

--- apps/game-master-node-server/drizzle/0002_sloppy_johnny_blaze.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "characters" ADD COLUMN "created_at" timestamp with time zone NOT NULL;--> statement-breakpoint
-ALTER TABLE "characters" ADD COLUMN "updated_at" timestamp with time zone NOT NULL;
--- apps/game-master-node-server/drizzle/0003_lonely_radioactive_man.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "factions" ADD COLUMN "created_at" timestamp with time zone NOT NULL;--> statement-breakpoint
-ALTER TABLE "factions" ADD COLUMN "updated_at" timestamp with time zone NOT NULL;
