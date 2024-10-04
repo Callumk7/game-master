f1ce3fc69a06aa8ad223f9144575ca771e95d6f9
Callum Kloos <Callumkloos@gmail.com>
2024-10-04 15:20:27 +0100

    remove db package from main branch

--- packages/db/src/db/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./characters";
-export * from "./factions";
-export * from "./plots";
-export * from "./sessions";
-export * from "./notes";
-export * from "./users";
-export * from "./locations";
--- packages/db/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./api";
-export * from "./db";
-export * from "./db/schemas";
-export * from "./types";
-export type * from "./types";
