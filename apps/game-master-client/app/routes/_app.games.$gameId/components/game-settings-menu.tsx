c4e337c232609038af8d99223aa307c9b7ca4992
Callum Kloos <Callumkloos@gmail.com>
2024-10-08 17:47:08 +0100

    add icons to the game settings menu, and navigate to the members area

--- apps/game-master-client/app/routes/_app.games.$gameId/components/game-settings-menu.tsx
+++ apps/game-master-client/app/routes/_app.games.$gameId/components/game-settings-menu.tsx
@@ -1,22 +1,47 @@
+import { FileTextIcon, PersonIcon, TrashIcon } from "@radix-ui/react-icons";
 import type { Id } from "@repo/api";
+import type { ReactNode } from "react";
+import type { MenuItemProps } from "react-aria-components";
 import { Button } from "~/components/ui/button";
 import { Menu, MenuItem, MenuPopover, MenuTrigger } from "~/components/ui/menu";
 
 interface GameSettingsMenuProps {
-	gameId: Id;
+  gameId: Id;
 }
 
 export function GameSettingsMenu({ gameId }: GameSettingsMenuProps) {
-	return (
-		<MenuTrigger>
-			<Button variant={"outline"}>Game Settings</Button>
-			<MenuPopover>
-				<Menu>
-					<MenuItem>Members</MenuItem>
-					<MenuItem>Update</MenuItem>
-					<MenuItem>Delete Game</MenuItem>
-				</Menu>
-			</MenuPopover>
-		</MenuTrigger>
-	);
+  return (
+    <MenuTrigger>
+      <Button variant={"outline"}>Game Settings</Button>
+      <MenuPopover>
+        <Menu>
+          <SettingsMenuItem href={`/games/${gameId}/members`} icon={<PersonIcon />}>
+            Members
+          </SettingsMenuItem>
+          <SettingsMenuItem icon={<FileTextIcon />}>
+            Manage Notes
+          </SettingsMenuItem>
+          <SettingsMenuItem icon={<TrashIcon />}>
+            Delete Game
+          </SettingsMenuItem>
+        </Menu>
+      </MenuPopover>
+    </MenuTrigger>
+  );
+}
+
+interface SettingsMenuItemProps extends MenuItemProps {
+  children: ReactNode;
+  icon: ReactNode;
+}
+
+export function SettingsMenuItem({ icon, children, ...props }: SettingsMenuItemProps) {
+  return (
+    <MenuItem {...props}>
+      <div className="flex items-center gap-x-2 w-full">
+        {icon}
+        {children}
+      </div>
+    </MenuItem>
+  );
 }
