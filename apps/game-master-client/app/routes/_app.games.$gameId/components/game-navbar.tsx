cccc183fa57749d263cd2baefcd3038f700a75c0
Callum Kloos <Callumkloos@gmail.com>
2024-10-08 17:39:14 +0100

    update game navbar to include active state and game homepage link

--- apps/game-master-client/app/routes/_app.games.$gameId/components/game-navbar.tsx
+++ apps/game-master-client/app/routes/_app.games.$gameId/components/game-navbar.tsx
@@ -1,41 +1,28 @@
-import { useParams } from "@remix-run/react";
+import { useLocation, useParams } from "@remix-run/react";
 import { Link } from "~/components/ui/link";
 import { GameSettingsMenu } from "./game-settings-menu";
 import { Button } from "~/components/ui/button";
 import { useIsRightSidebarOpen, useSetRightSidebarOpen } from "~/store/selection";
 import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
 
-const createLinks = (gameId: string) => {
-  return [
-    {
-      label: "Notes",
-      href: `/games/${gameId}/notes`,
-    },
-    {
-      label: "Characters",
-      href: `/games/${gameId}/characters`,
-    },
-    {
-      label: "Factions",
-      href: `/games/${gameId}/factions`,
-    },
-    {
-      label: "Collections",
-      href: `/games/${gameId}/collections`,
-    },
-  ];
-};
-
 export function GameNavbar() {
   const params = useParams();
+  const { pathname } = useLocation();
   const links = createLinks(params.gameId!);
   const isRightSidebarOpen = useIsRightSidebarOpen();
   const setIsRightSidebarOpen = useSetRightSidebarOpen();
   return (
-    <nav className="flex w-full p-4 justify-between items-center" aria-label="Game navigation bar">
-      <div className="flex">
+    <nav
+      className="flex w-full p-4 justify-between items-center"
+      aria-label="Game navigation bar"
+    >
+      <div className="flex gap-1">
         {links.map((link) => (
-          <Link key={link.label} href={link.href} variant={"ghost"}>
+          <Link
+            key={link.label}
+            href={link.href}
+            variant={pathname.endsWith(link.href) ? "secondary" : "ghost"}
+          >
             {link.label}
           </Link>
         ))}
@@ -54,3 +41,28 @@ export function GameNavbar() {
     </nav>
   );
 }
+
+const createLinks = (gameId: string) => {
+  return [
+    {
+      label: "Game",
+      href: `/games/${gameId}`,
+    },
+    {
+      label: "Notes",
+      href: `/games/${gameId}/notes`,
+    },
+    {
+      label: "Characters",
+      href: `/games/${gameId}/characters`,
+    },
+    {
+      label: "Factions",
+      href: `/games/${gameId}/factions`,
+    },
+    {
+      label: "Collections",
+      href: `/games/${gameId}/collections`,
+    },
+  ];
+};
