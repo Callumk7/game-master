f0d473e1dacdca5bf7bc2c246be5bc380621a39a
Callum Kloos <Callumkloos@gmail.com>
2024-10-08 18:43:45 +0100

    fix background on text area

--- apps/game-master-client/app/components/ui/textfield.tsx
+++ apps/game-master-client/app/components/ui/textfield.tsx
@@ -20,7 +20,7 @@ const Input = ({ className, ...props }: AriaInputProps) => {
 		<AriaInput
 			className={composeRenderProps(className, (className) =>
 				cn(
-					"flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground",
+					"flex h-9 w-full rounded-md border border-input bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground",
 					/* Disabled */
 					"data-[disabled]:cursor-not-allowed data-[disabled]:opacity-50",
 					/* Focused */
