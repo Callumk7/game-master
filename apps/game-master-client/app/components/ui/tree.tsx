df8b81db11a51a4c2fb3bb74e3a9a41d1c775c0a
Callum Kloos <Callumkloos@gmail.com>
2024-10-16 16:17:28 +0100

    first draft: tree component

---
+++ apps/game-master-client/app/components/ui/tree.tsx
@@ -0,0 +1,50 @@
+import { cn } from "callum-util";
+import {
+	UNSTABLE_Tree as AriaTree,
+	UNSTABLE_TreeItem as AriaTreeItem,
+	UNSTABLE_TreeItemContent as AriaTreeItemContent,
+	type TreeProps as AriaTreeProps,
+	type TreeItemProps as AriaTreeItemProps,
+	composeRenderProps,
+	Collection,
+} from "react-aria-components";
+
+function Tree<T extends object>({ className, ...props }: AriaTreeProps<T>) {
+	return (
+		<AriaTree
+			className={composeRenderProps(className, (className) =>
+				cn(
+					className,
+					"group overflow-auto w-full p-1 text-popover-foreground outline-none",
+				),
+			)}
+			{...props}
+		/>
+	);
+}
+
+interface TreeItemProps<T> extends AriaTreeItemProps<T> {
+	itemChildren: T[] | undefined;
+	renderFunction: (item: T) => JSX.Element;
+}
+const TreeItem = <T extends object>({
+	className,
+	children,
+	itemChildren,
+	renderFunction,
+	...props
+}: TreeItemProps<T>) => {
+	return (
+		<AriaTreeItem
+			className={composeRenderProps(className, (className) => cn(className, ""))}
+			{...props}
+		>
+			<AriaTreeItemContent>{children}</AriaTreeItemContent>
+			<Collection items={itemChildren}>{renderFunction}</Collection>
+		</AriaTreeItem>
+	);
+};
+
+const TreeItemContent = AriaTreeItemContent;
+
+export { Tree, TreeItem, TreeItemContent };
