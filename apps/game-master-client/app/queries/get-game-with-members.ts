d1e3dfa9325866ae6eccd86158c5e9a97bd1ad34
Callum Kloos <Callumkloos@gmail.com>
2024-10-24 21:22:39 +0100

    add type annotation (temp)

--- apps/game-master-client/app/queries/get-game-with-members.ts
+++ apps/game-master-client/app/queries/get-game-with-members.ts
@@ -1,9 +1,12 @@
-import type { SDK } from "@repo/api";
+import type { SDK, User } from "@repo/api";
 import { useQuery } from "@tanstack/react-query";
 
 export const useGetGameWithMembers = (gameId: string) => {
 	return useQuery({
 		queryKey: ["games", gameId],
-		queryFn: async () => fetch(`/members/${gameId}`).then((result) => result.json()),
+		queryFn: async () =>
+			fetch(`/members/${gameId}`).then(
+				(result) => result.json() as Promise<{members: User[]}>,
+			),
 	});
 };
