3114e4646895eb12bf0a26717bf96cdb5646cd2a
Callum Kloos <Callumkloos@gmail.com>
2024-10-28 14:11:42 +0000

    use character table when viewing primary faction

--- apps/game-master-client/app/routes/_app.games.$gameId.characters.$charId.factions/route.tsx
+++ apps/game-master-client/app/routes/_app.games.$gameId.characters.$charId.factions/route.tsx
@@ -6,6 +6,8 @@ import { validateUser } from "~/lib/auth.server";
 import { typedjson, useTypedLoaderData } from "remix-typedjson";
 import { FactionTable } from "~/components/tables/faction-table";
 import { LinkFactionDialog } from "./components/link-faction-dialog";
+import { Text } from "~/components/ui/typeography";
+import { PrimaryFaction } from "./components/primary-faction";
 
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   const userId = await validateUser(request);
@@ -14,10 +16,11 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
     gameId: z.string(),
   });
   const api = createApi(userId);
+  const primaryFaction = await api.characters.getPrimaryFaction(charId);
   const charFactions = await api.characters.getFactions(charId);
   const allFactions = await api.factions.getAllGameFactions(gameId);
 
-  return typedjson({ charFactions, allFactions });
+  return typedjson({ charFactions, allFactions, primaryFaction });
 };
 
 export const action = async ({ request, params }: ActionFunctionArgs) => {
@@ -43,9 +46,11 @@ export const action = async ({ request, params }: ActionFunctionArgs) => {
 };
 
 export default function CharacterFactionsRoute() {
-  const { allFactions, charFactions } = useTypedLoaderData<typeof loader>();
+  const { allFactions, charFactions, primaryFaction } =
+    useTypedLoaderData<typeof loader>();
   return (
     <div className="space-y-4">
+      {primaryFaction ? <PrimaryFaction faction={primaryFaction} members={primaryFaction.members} /> : null}
       <LinkFactionDialog allFactions={allFactions} />
       <FactionTable factions={charFactions} />
     </div>
