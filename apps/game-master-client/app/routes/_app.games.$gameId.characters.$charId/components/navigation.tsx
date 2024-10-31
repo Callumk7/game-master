8325956eea657fed9ed406d67d2a45b508447ebd
Callum Kloos <Callumkloos@gmail.com>
2024-10-31 10:32:04 +0000

    wrap char navigation with nav

--- apps/game-master-client/app/routes/_app.games.$gameId.characters.$charId/components/navigation.tsx
+++ apps/game-master-client/app/routes/_app.games.$gameId.characters.$charId/components/navigation.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "@remix-run/react";
 import { Button } from "~/components/ui/button";
 import { Toolbar } from "~/components/ui/toolbar";
+import { characterHref } from "~/util/generate-hrefs";
 
 interface CharacterNavigationProps {
   charId: string;
@@ -12,19 +13,27 @@ interface CharacterNavigationProps {
 export function CharacterNavigation({ charId, gameId }: CharacterNavigationProps) {
   const navigate = useNavigate();
   return (
-    <Toolbar>
-      <Button
-        onPress={() => navigate(`/games/${gameId}/characters/${charId}`)}
-        variant={"link"}
-      >
-        Overview
-      </Button>
-      <Button onPress={() => navigate("factions", { relative: "path" })} variant={"link"}>
-        Factions
-      </Button>
-      <Button onPress={() => navigate("relations", { relative: "path" })} variant={"link"}>
-        Relations
-      </Button>
-    </Toolbar>
+    <nav>
+      <Toolbar>
+        <Button
+          onPress={() => navigate(characterHref(gameId, charId))}
+          variant={"link"}
+        >
+          Overview
+        </Button>
+        <Button
+          onPress={() => navigate("factions", { relative: "path" })}
+          variant={"link"}
+        >
+          Factions
+        </Button>
+        <Button
+          onPress={() => navigate("relations", { relative: "path" })}
+          variant={"link"}
+        >
+          Relations
+        </Button>
+      </Toolbar>
+    </nav>
   );
 }
