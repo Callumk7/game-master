d6f9b4c34a8b2484813f5b269b4ce5e4b33c3492
Callum Kloos <Callumkloos@gmail.com>
2024-11-18 10:57:50 +0000

    basic re-route and game display

---
+++ apps/game-master-client/app/routes/_app.games._index/components/home-navigation.tsx
@@ -0,0 +1,19 @@
+import type { User } from "@repo/api";
+import { Link } from "~/components/ui/link";
+
+interface HomeNavigationProps {
+  user: User;
+}
+
+export function HomeNavigation({ user }: HomeNavigationProps) {
+  return (
+    <nav className="py-1 flex items-center">
+      <Link href="/games/new" variant={"link"}>
+        Create Game
+      </Link>
+      <Link href="/user" variant={"link"}>
+        {user.username}'s Settings
+      </Link>
+    </nav>
+  );
+}
