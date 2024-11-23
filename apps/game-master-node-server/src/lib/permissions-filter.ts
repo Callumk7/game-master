a6f26cbee704ad3af7de24cdeebf353dfc32db3b
Callum Kloos <Callumkloos@gmail.com>
2024-11-23 17:06:08 +0000

    remove console log for permission calculations

--- apps/game-master-node-server/src/lib/permissions-filter.ts
+++ apps/game-master-node-server/src/lib/permissions-filter.ts
@@ -11,23 +11,19 @@ export function filterItems(
 	{ ownerId, visibility, permissions }: FilterData,
 ) {
 	if (userId === ownerId) {
-		console.log("user is the owner");
 		return true;
 	}
 
 	if (permissions) {
 		const userPermission = permissions.find((p) => p.userId === userId);
 		if (userPermission && userPermission.permission !== "none") {
-			console.log("user does not have a none permission");
 			return true;
 		}
 	}
 
 	if (visibility !== "private") {
-		console.log("visibility is not private");
 		return true;
 	}
 
-	console.log("entity is not visibile");
 	return false;
 }
