520808c6b32860b8921124b693ddb70d68f16bc0
Callum Kloos <Callumkloos@gmail.com>
2024-11-26 12:02:19 +0000

    centralise update note function

--- apps/game-master-client/app/actions/notes.server.ts
+++ apps/game-master-client/app/actions/notes.server.ts
@@ -1,6 +1,13 @@
-import type { SDK } from "@repo/api";
+import { updateNoteContentSchema, type SDK } from "@repo/api";
 import { typedjson } from "remix-typedjson";
+import { parseForm } from "zodix";
 import { successRedirect } from "~/lib/navigation";
+
+export const updateNote = async (request: Request, api: SDK, noteId: string) => {
+	const data = await parseForm(request, updateNoteContentSchema);
+	const result = await api.notes.update(noteId, data);
+	return typedjson(result);
+};
 
 export const deleteNote = async (api: SDK, noteId: string, gameId: string) => {
 	const result = await api.notes.delete(noteId);
