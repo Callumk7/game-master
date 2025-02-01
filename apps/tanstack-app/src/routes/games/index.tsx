52766f26bae9fcdd9781e92fa0eadd24b8983ecb
Callum Kloos <Callumkloos@gmail.com>
2025-02-01 16:37:52 +0000

    remove tanstack experiment

--- apps/tanstack-app/src/routes/games/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/games/")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div className="bg-black">Hello "/games/"!</div>;
-}
